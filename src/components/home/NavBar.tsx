2755af84827ea5b665dfc57081f9f48f757c96a2
kinn <kinn@kinn.kr>
2024-07-06 11:45:47 +0900

    Modify NavBar

--- src/components/home/NavBar.tsx
+++ src/components/home/NavBar.tsx
@@ -13,20 +13,36 @@ function NavBar() {
                 </a>
 
                 <div className="topnav">
+                    <a href="../greeting" title="인사말">
+                        인사말
+                    </a>
+
                     <a href="../#works" title="업무분야">
                         업무분야
                     </a>
-                    <a href="../success" title="승소사례">
-                        승소사례
-                    </a>
+
                     <a href="../members" title="구성원">
                         구성원
                     </a>
+
+                    <a href="../success" title="승소사례">
+                        승소사례
+                    </a>
+
+                    <a href="../success" title="고객후기">
+                        고객후기
+                    </a>
+
                     <a href="../media" title="미디어 소개">
                         미디어 소개
                     </a>
-                    <a href="../address" title="오시는 길">
+
+                    <a href="../#address" title="오시는 길">
                         오시는 길
+                    </a>
+
+                    <a href="../#consult" title="상담문의">
+                        상담문의
                     </a>
                 </div>
             </nav>
