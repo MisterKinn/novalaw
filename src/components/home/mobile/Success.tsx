aa5beee5ca0ed86083a398d79d5cf64f644d844b
kinn <kinn@kinn.kr>
2024-07-08 16:18:02 +0900

    Add Mobile Success

--- src/components/home/mobile/Success.tsx
+++ src/components/home/mobile/Success.tsx
@@ -1,41 +1,33 @@
+import "@/styles/Mobile/success.css";
+
 function Success() {
     return (
-        <div>
-            <h1 className="mobile-success-title" data-aos="fade-up">
-                <span className="white">전문적인 구성원</span>들과
+        <div id="mobile-success">
+            <h2 className="mobile-success-title">
+                전문적인 구성원들과
                 <br />
-                <span className="white">여러 건의 승소사례</span>를
+                여러 건의 승소사례를
                 <br />
-                다양한 분야에서
-                <br />
-                쌓아냈습니다.
-            </h1>
+                다양한 분야에서 쌓아왔습니다.
+            </h2>
 
-            <div className="success-button" data-aos="fade-up">
-                <a href="/members" target="_blank" title="구성원 더 알아보기">
-                    <button
-                        className="learn-more learn-more-mobile"
-                        role="button"
-                    >
-                        <span className="mobile-learn-more-text">
-                            구성원 더 알아보기
-                        </span>
-                    </button>
+            <div className="mobile-success-button">
+                <a href="/members" title="구성원 더 알아보기">
+                    <div className="mobile-learn-more">
+                        <p>구성원 더 알아보기 {">"}</p>
+                    </div>
                 </a>
 
-                <div className="margin"></div>
+                <div className="small-margin" />
 
-                <a href="/success" target="_blank" title="승소사례 더 알아보기">
-                    <button
-                        className="learn-more learn-more-mobile"
-                        role="button"
-                    >
-                        <span className="mobile-learn-more-text">
-                            승소사례 더 알아보기
-                        </span>
-                    </button>
+                <a href="/success" title="승소사례 더 알아보기">
+                    <div className="mobile-learn-more">
+                        <p>승소사례 더 알아보기 {">"}</p>
+                    </div>
                 </a>
             </div>
+
+            <div className="mobile-margin">ㅤ</div>
         </div>
     );
 }
--- src/components/home/mobile/Works.tsx
+++ src/components/home/mobile/Works.tsx
@@ -1,149 +1,123 @@
+import "@/styles/Mobile/work.css";
+
 function Works() {
     return (
         <div id="works">
-            <h2 className="mobile-introduce" data-aos="fade-up">
-                업무 소개
-            </h2>
-            <div className="container" data-aos="fade-up">
-                <div className="spec">
-                    <div className="card-container mobile-card-container">
+            <img
+                src="img/work-bg-mobile.png"
+                alt="노바 업무분야"
+                className="mobile-works-bg"
+            />
+
+            <div className="mobile-works-component">
+                <h2 className="mobile-works-title" data-aos="fade-up">
+                    노바 업무분야
+                </h2>
+                <h3 className="mobile-works-subtitle" data-aos="fade-up">
+                    풍부한 사건처리 데이터와 노하우를 바탕으로
+                    <br />
+                    의뢰인의 편에서 솔루션을 제시하는
+                    <br />
+                    노바법률사무소입니다.
+                </h3>
+
+                <a
+                    href="/works"
+                    title="업무분야 더 알아보기"
+                    className="mobile-works-button"
+                >
+                    <div data-aos="fade-up">
+                        <p>업무분야 더 알아보기 {">"}</p>
+                    </div>
+                </a>
+
+                <div className="mobile-container" data-aos="fade-up">
+                    <div className="mobile-card-container">
                         <div>
-                            <div className="column-center">
+                            <div className="mobile-card-img">
                                 <img
                                     loading="lazy"
-                                    src="img/handcuffs.png"
-                                    className="card-img"
+                                    src="img/work_1.png"
+                                    className="mobile-cardimg"
                                 />
-                                <span className="card-title">
-                                    <strong> 형사사건</strong>
-                                </span>
                             </div>
+                            <div className="mobile-card-title">형사사건</div>
                             <div className="mobile-card-text">
-                                <strong>
-                                    수사단계부터 재판까지
-                                    <br />
-                                    의뢰인의 편에서
-                                    <br />
-                                    <span className="white">최선의 결과</span>를
-                                    위해 노력합니다.
-                                </strong>
+                                수사단계부터 재판까지 의뢰인의 편에서
+                                <br />
+                                최선의 결과를 위해 노력합니다.
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
 
-            <div className="container" data-aos="fade-up">
-                <div className="spec">
-                    <div className="card-container mobile-card-container">
+                    <div className="mobile-card-container">
                         <div>
-                            <div className="column-center">
+                            <div className="mobile-card-img">
                                 <img
                                     loading="lazy"
-                                    src="img/civil.png"
-                                    className="card-img"
+                                    src="img/work_2.png"
+                                    className="mobile-cardimg"
                                 />
-                                <span className="card-title">
-                                    <strong> 민사소송</strong>
-                                </span>
                             </div>
+                            <div className="mobile-card-title">민사소송</div>
                             <div className="mobile-card-text">
-                                <strong>
-                                    의뢰인의 소중한 권리와
-                                    <br />
-                                    자산 보호를 위해{" "}
-                                    <span className="white">체계적</span>이고
-                                    <br />
-                                    <span className="white">전략적</span>으로
-                                    접근합니다.
-                                </strong>
+                                의뢰인의 소중한 권리와 자산 보호를 위해
+                                <br />
+                                체계적이고 전략적으로 접근합니다.
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
 
-            <div className="container" data-aos="fade-up">
-                <div className="spec">
-                    <div className="card-container mobile-card-container">
+                    <div className="mobile-card-container">
                         <div>
-                            <div className="column-center">
+                            <div className="mobile-card-img">
                                 <img
                                     loading="lazy"
-                                    src="img/hammer.png"
-                                    className="card-img"
+                                    src="img/work_3.png"
+                                    className="mobile-cardimg"
                                 />
-                                <span className="card-title">
-                                    <strong> 행정쟁송</strong>
-                                </span>
                             </div>
+                            <div className="mobile-card-title">행정쟁송</div>
                             <div className="mobile-card-text">
-                                <strong>
-                                    국가로부터 권리를 침해받았을
-                                    <br />
-                                    경우 구제받을 수 있도록 행정쟁송의
-                                    <br />
-                                    <span className="white">조력자</span>가
-                                    되어드립니다.
-                                </strong>
+                                국가로부터 권리를 침해받았을 경우
+                                <br />
+                                구제받을 수 있도록 조력자가 되어드립니다.
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
 
-            <div className="container" data-aos="fade-up">
-                <div className="spec">
-                    <div className="card-container mobile-card-container">
+                    <div className="mobile-card-container">
                         <div>
-                            <div className="column-center">
+                            <div className="mobile-card-img">
                                 <img
                                     loading="lazy"
-                                    src="img/labor.png"
-                                    className="card-img"
+                                    src="img/work_4.png"
+                                    className="mobile-cardimg"
                                 />
-                                <span className="card-title">
-                                    <strong> 노동쟁송</strong>
-                                </span>
                             </div>
+                            <div className="mobile-card-title">노동쟁송</div>
                             <div className="mobile-card-text">
-                                <strong>
-                                    고문 노무사님과 함께{" "}
-                                    <span className="white">당연한 권리</span>를
-                                    <br />
-                                    누릴 수 있도록 돕습니다.
-                                </strong>
+                                고문 노무사님과 함께
+                                <br />
+                                당연한 권리를 누릴 수 있도록 돕습니다.
                             </div>
                         </div>
                     </div>
-                </div>
-            </div>
 
-            <div className="container" data-aos="fade-up">
-                <div className="spec">
-                    <div className="card-container mobile-card-container">
+                    <div className="mobile-card-container">
                         <div>
-                            <div className="column-center">
+                            <div className="mobile-card-img">
                                 <img
                                     loading="lazy"
-                                    src="img/consulting.png"
-                                    className="card-img"
+                                    src="img/work_5.png"
+                                    className="mobile-cardimg"
                                 />
-                                <span className="card-title">
-                                    <strong> 법률자문</strong>
-                                </span>
                             </div>
+                            <div className="mobile-card-title">법률자문</div>
                             <div className="mobile-card-text">
-                                <strong>
-                                    정기적인 법률자문이 필요한 경우
-                                    <br />
-                                    <span className="white">
-                                        믿을 수 있는
-                                    </span>{" "}
-                                    법률자문
-                                    <br />
-                                    서비스를 진행합니다.
-                                </strong>
+                                정기적인 법률자문이 필요한 경우
+                                <br />
+                                믿을 수 있는 법률자문 서비스를 제공합니다.
                             </div>
                         </div>
                     </div>
