f6ce7c258e4a7445710c1464e49d0064abb24b58
kinn <kinn@kinn.kr>
2024-07-09 12:17:19 +0900

    Add Layouts to Each Page

---
+++ src/app/speciality/layout.tsx
@@ -0,0 +1,27 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+    title: "노바의 특별함ㅣ노바법률사무소",
+    description: "법률서비스의 새로운 시작을 알리는 노바법률사무소입니다.",
+    icons: {
+        icon: "img/novalaw-icon.png",
+    },
+    other: {
+        url: "img/posco.png",
+    },
+};
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <html lang="en">
+            <body className={inter.className}>{children}</body>
+        </html>
+    );
+}
