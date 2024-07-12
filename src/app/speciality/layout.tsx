2010b656afbad6a83cdad962808de70a6f2c217a
kinn <kinn@kinn.kr>
2024-07-12 19:30:50 +0900

    Font to every Page

--- src/app/speciality/layout.tsx
+++ src/app/speciality/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import "./global.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +22,9 @@ export default function RootLayout({
 }>) {
     return (
         <html lang="en">
-            <body className={inter.className}>{children}</body>
+            <body className={inter.className} id="main">
+                {children}
+            </body>
         </html>
     );
 }
