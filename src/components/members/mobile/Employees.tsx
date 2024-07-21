38969857ff54b20834654f1b3747a8e9be343ef3
kinn <kinn@kinn.kr>
2024-07-21 15:03:10 +0900

    Modify Mobile Members Page

--- src/components/members/mobile/Employees.tsx
+++ src/components/members/mobile/Employees.tsx
@@ -15,17 +15,19 @@ function Employees() {
                                 className="mobile-employees-img"
                             />
                             <div className="mobile-career-text">
-                                서울대학교 법과대학 (법학사, 우등졸업)
+                                서울대학교 법과대학 (학사, 우등졸업)
                                 <br />
-                                고려대학교 법학전문대학원 (법학전문석사)
+                                고려대학교 법학전문대학원 (전문석사)
                                 <br />
                                 제3회 변호사시험 합격
                                 <br />
-                                Y&K 법률사무소 변호사
+                                前 Y&K 법률사무소 변호사
                                 <br />
-                                법무법인 전문 변호사
+                                前 법무법인(유한) 주원 변호사
                                 <br />
-                                법무법인(유한) 주원 변호사
+                                現 법무법인 전문 변호사
+                                <br />
+                                現 노바법률사무소 파트너 변호사
                                 <br />
                                 <br />ㅤ
                             </div>
@@ -47,15 +49,39 @@ function Employees() {
                         className="mobile-employees-img"
                     />
                     <div className="mobile-career-text">
-                        現 노바법률사무소 고문변호사
+                        서울대학교 법과대학 (학사)
                         <br />
-                        現 엠디엠플러스 준법지원팀장
+                        연세대학교 법학전문대학원 졸업 (전문석사)
                         <br />
                         前 GS건설 국내법무팀 변호사
+                        <br />現 노바법률사무소 고문변호사
+                    </div>
+                </div>
+            </div>
+
+            <div className="mobile-container" data-aos="fade-up">
+                <div className="mobile-card-container">
+                    <div className="mobile-card-title">
+                        <h1>서예은</h1>
+                        <h2>자문회계사</h2>
+                    </div>
+                </div>
+
+                <div className="mobile-career-container">
+                    <img
+                        src="img/seo-yeeun.png"
+                        alt="서예은 자문회계사"
+                        className="mobile-employees-img"
+                    />
+                    <div className="mobile-career-text">
+                        균관대학교 경제학과 졸업 (학사)
                         <br />
-                        서울대학교 법학부 졸업(학사)
-                        <br />
-                        연세대학교 법학전문대학원 졸업(석사){" "}
+                        2012년 공인회계사 합격
+                        <br />前 김&장 법률사무소 회계사
+                        <br />前 KPMG 삼정회계법인 세무자문본부
+                        <br />前 Deloitte 안진회계법인 회계감사본부
+                        <br />現 회계법인 마일스톤 회계사
+                        <br />現 노바법률사무소 자문회계사
                     </div>
                 </div>
             </div>
@@ -76,10 +102,8 @@ function Employees() {
                     />
                     <div className="mobile-career-text">
                         現 노바법률사무소 자문세무사
-                        <br />
-                        現 회계법인 더봄 홍대점 부대표 회계사
-                        <br />
-                        前 롯데쇼핑 백화점사업 본부
+                        <br />現 세무법인 더봄 홍대점 부대표 세무사
+                        <br />前 롯데쇼핑 백화점사업본부
                         <br />
                         경영기획팀 / 점포 영업총괄팀
                         <br />
@@ -109,56 +133,20 @@ function Employees() {
                             />
                             <div className="mobile-career-text">
                                 現 노바법률사무소 자문노무사
-                                <br />
-                                現 노무법인 리담 광화문 본사 노무사
-                                <br />
-                                前 중소기업벤처기업부 비즈니스지원단 현장클리닉
-                                <br />
-                                前 고용노동부 일터혁신 컨설턴트
-                                <br />
-                                前 공인노무사회 노동시간 단축 전문 컨설턴트
-                                <br />
-                                前 대한민국 공군 군수사령부 인사장교
-                                <br />
-                                前 현대차그룹 현대엔지니어링 사내노무사
+                                <br />現 노무법인 리담 광화문 본사 노무사
+                                <br />前 중소벤처기업부 비즈니스지원단
+                                현장클리닉
+                                <br />前 고용노동부 일터혁신 컨설턴트
+                                <br />前 공인노무사회 노동시간 단축 전문
+                                컨설턴트
+                                <br />前 대한민국 공군 군수사령부 인사장교
+                                <br />前 현대차그룹 현대엔지니어링 사내 노무사
                                 <br />
                                 고려대학교 경영대학원 석사과정
                                 <br />
                                 경북대학교 사범대학
                             </div>
                         </div>
-                    </div>
-                </div>
-            </div>
-
-            <div className="mobile-container" data-aos="fade-up">
-                <div className="mobile-card-container">
-                    <div className="mobile-card-title">
-                        <h1>서예은</h1>
-                        <h2>자문회계사</h2>
-                    </div>
-                </div>
-
-                <div className="mobile-career-container">
-                    <img
-                        src="img/seo-yeeun.png"
-                        alt="서예은 자문회계사"
-                        className="mobile-employees-img"
-                    />
-                    <div className="mobile-career-text">
-                        現 노바법률사무소 자문회계사
-                        <br />
-                        現 회계법인 마일스톤 회계사
-                        <br />
-                        前 김&장 법률사무소 회계사
-                        <br />
-                        前 KPMG 삼정회계법인 세무자문본부
-                        <br />
-                        前 Deloitte 안전회계법인 회계감사본부
-                        <br />
-                        2012년 공인회계사 합격
-                        <br />
-                        성균관대학교 경제학과 졸업
                     </div>
                 </div>
             </div>
@@ -177,23 +165,19 @@ function Employees() {
                             className="mobile-employees-img"
                         />
                         <div className="mobile-career-text">
-                            現 노바법률사무소 자문의
-                            <br />
-                            現 오늘도 365의원 원장
-                            <br />
-                            前 한양대학교의료원 외래교수
-                            <br />
-                            前 EM365 영종점 부원장
-                            <br />
-                            前 연세메디람의원 호스피스전문센터 진료과장
-                            <br />
-                            前 오산한국병원 응급의학과 진료과장
-                            <br />
-                            한양대학교 명지병원 권역응급의료센터 전공의
+                            관동대학교 의과대학 졸업
                             <br />
                             국제성모병원 수련의 수료
                             <br />
-                            관동대학교 의과대학 졸업
+                            한양대학교 명지병원 권역응급의료센터 전공의
+                            <br />
+                            前 한양대학교의료원 외래교수
+                            <br />前 연세메디람의원 호스피스전문 센터 진료과장
+                            <br />前 오산한국병원 응급의학과 진료과장
+                            <br />
+                            前 EM365 영종점 부원장
+                            <br />現 오늘도 365의원 원장
+                            <br />現 노바법률사무소 자문의
                         </div>
                     </div>
                 </div>
--- src/components/members/mobile/Intro.tsx
+++ src/components/members/mobile/Intro.tsx
@@ -8,18 +8,16 @@ function Intro() {
             />
 
             <div className="mobile-members-container" data-aos="fade-up">
-                <h2 className="mobile-members-title">구성원 소개</h2>
-                <h3 className="mobile-members-subtitle">
-                    <span className="mobile-white">전문적인 구성원</span>들로
-                    이루어진 <br />
+                <h2>구성원 소개</h2>
+                <h3>
+                    전문 변호사들로 이루어진 <br />
                     <span className="mobile-white">노바법률사무소</span>입니다.
-                    <br />
-                    <span className="mobile-white">법률적 도움</span>이 필요하실
-                    때는
-                    <br />
-                    <span className="mobile-white">노바법률사무소</span>와
-                    함께하세요.
                 </h3>
+                <h4>
+                    도움이 필요하실 때는
+                    <br />
+                    노바의 전문 변호사들과 함께하세요.
+                </h4>
             </div>
         </div>
     );
